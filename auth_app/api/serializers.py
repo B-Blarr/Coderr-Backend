862c7a721807613aa59f924520274303536529a6
Benjamin Blarr <b.blarr@gmx.de>
2026-07-01 14:50:02 +0200

    Add login serializer for user authentication with validation

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -26,3 +26,19 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):                
         validated_data.pop('repeated_password')
         return User.objects.create_user(**validated_data)
+    
+
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+
+        user = authenticate(
+            username=data['username'],
+            password=data['password'])
+        if not user:
+            raise serializers.ValidationError('Invalid credentials')
+        data['user'] = user
+        return data
