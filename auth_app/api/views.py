4b247c237e67d189c45e777b24b52bcc37c88098
Benjamin Blarr <b.blarr@gmx.de>
2026-07-01 14:53:38 +0200

    Add custom login view with token generation and error handling

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -2,8 +2,9 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer
 
 
 class RegistrationView(APIView):
@@ -28,3 +29,25 @@ class RegistrationView(APIView):
             return Response(
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class CustomLoginView(ObtainAuthToken):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+
+        serializer = RegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            data = {
+                'token': token.key,
+                'username': user.username,
+                'email': user.email,
+                'user_id': user.id,
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
