a42b410db89e35899d560c2d2c27e66d4c5cd856
Benjamin Blarr <b.blarr@gmx.de>
2026-07-06 15:18:12 +0200

    Implement OfferViewSet with serializer selection and user assignment on creation

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from offers_app.models import Offer
+from .serializers import OfferCreateSerializer, OfferSerializer
 
-# Create your views here.
+
+class OfferViewSet(viewsets.ModelViewSet):
+    queryset = Offer.objects.all()
+    
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return OfferCreateSerializer
+        return OfferSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
