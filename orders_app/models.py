e0fbe20a9474736b8f2eee98d688ab366605a9e6
Benjamin Blarr <b.blarr@gmx.de>
2026-07-09 00:02:01 +0200

    Add Order model with fields and choices for type and status

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from auth_app.models import User
 
-# Create your models here.
+class Order(models.Model):
+
+    TYPE_CHOICES = [
+        ('basic', 'Basic'),
+        ('standard', 'Standard'),
+        ('premium', 'Premium')
+    ]
+
+    STATUS_CHOICES = [
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    customer_user = models.ForeignKey(
+        User, related_name='orders_as_customer', on_delete=models.CASCADE)
+    business_user = models.ForeignKey(
+        User, related_name='orders_as_business', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    status = models.CharField(
+        max_length=30, choices=STATUS_CHOICES, default='in_progress')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+        def __str__(self):
+            return self.title
