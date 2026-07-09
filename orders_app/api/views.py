69fe403e31a889383f53ba4cb9080a37c86e5968
Benjamin Blarr <b.blarr@gmx.de>
2026-07-09 13:52:11 +0200

    Add OrderViewSet to manage order operations and filter by user

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework import viewsets
+from rest_framework import generics
+from .serializers import OrderSerializer
+from orders_app.models import Order
 
-# Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+
+    def get_serializer_class(self):
+        # if self.action in ['create', 'update', 'partial_update']:
+        #     return OrderCreateSerializer
+        return OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(customer_user=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Order.objects.filter(
+            Q(customer_user=user) | Q(business_user=user))
