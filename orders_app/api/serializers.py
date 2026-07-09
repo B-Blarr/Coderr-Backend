43047fde2e82d71e5461983e7930efbdc0c85141
Benjamin Blarr <b.blarr@gmx.de>
2026-07-09 12:11:13 +0200

    Add OrderSerializer to handle order creation with offer details

---
+++ orders_app/api/serializers.py
@@ -0,0 +1,33 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+from orders_app.models import Order
+from offers_app.models import OfferDetail
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    offer_detail_id = serializers.IntegerField(write_only=True)
+  
+    class Meta:
+        model = Order
+        fields = ['id', 'customer_user', 'business_user','title', 'revisions',
+                  'delivery_time_in_days', 'price', 'features', 'offer_type',
+                  'status', 'created_at', 'updated_at', 'offer_detail_id']
+        
+        read_only_fields = ['customer_user', 'business_user', 'title',
+                            'revisions','delivery_time_in_days', 'price', 
+                            'features','offer_type', 'status']
+
+    def create(self, validated_data):
+        offer_detail_id = validated_data.pop('offer_detail_id')      
+        detail = get_object_or_404(OfferDetail, id=offer_detail_id)   
+        return Order.objects.create(                                   
+            customer_user=validated_data['customer_user'],
+            business_user=detail.offer.user,                 
+            title=detail.title,
+            revisions=detail.revisions,
+            delivery_time_in_days=detail.delivery_time_in_days,
+            price=detail.price,
+            features=detail.features,
+            offer_type=detail.offer_type                                                       
+    )
+
