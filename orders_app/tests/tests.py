640cf12357debdcce13de1146c43280ddc2fc6d4
Benjamin Blarr <b.blarr@gmx.de>
2026-07-10 13:33:33 +0200

    Add initial test setup for Order model with user and offer creation

--- orders_app/tests/tests.py
+++ orders_app/tests/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+from orders_app.models import Order
+from offers_app.models import Offer, OfferDetail
 
-# Create your tests here.
+User = get_user_model()
+
+class OrderTests(APITestCase):
+
+    def setUp(self):
+
+        self.business = User.objects.create_user(
+            username='busi', password='pass1234', type='business')
+        self.other_business = User.objects.create_user(
+            username='busi2', password='pass1234', type='business')
+        self.customer = User.objects.create_user(
+            username='custo', password='pass1234', type='customer')
+        self.admin = User.objects.create_user(
+            username='admin', password='pass1234', type='customer', is_staff=True)
+        
+        self.offer = Offer.objects.create(
+            user=self.business, title='Test Offer', description='description')
+        self.detail = OfferDetail.objects.create(
+            offer=self.offer, title='Basic', revisions=1, delivery_time_in_days=5,
+            price='100.00', features=['feature'], offer_type='basic')
+
+        self.order = Order.objects.create(
+            customer_user=self.customer, business_user=self.business,
+            title='Basic', revisions=1, delivery_time_in_days=5, price='100.00',
+            features=['feature'], offer_type='basic', status='in_progress')
