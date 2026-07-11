4a2f393850722bffa1d5a4fb1f08ee69fb5ba8bb
Benjamin Blarr <b.blarr@gmx.de>
2026-07-11 13:40:51 +0200

    Add Review model with fields for business user, reviewer, rating, and description

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from auth_app.models import User
 
+class Review(models.Model):
 
+    business_user = models.ForeignKey(User, related_name='reviews_as_business')
+    reviewer = models.ForeignKey(
+        User, related_name='orders_as_reviewer', on_delete=models.CASCADE)
+    rating = models.IntegerField(1-9)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return self.description
