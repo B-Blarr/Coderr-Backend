9b7715371e776be1b3b21fb70bf49501849d8296
Benjamin Blarr <b.blarr@gmx.de>
2026-07-12 11:44:34 +0200

    Add reviews API with ReviewViewSet and router configuration

--- reviews_app/api/views.py
+++ reviews_app/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ReviewSerializer, ReviewUpdateSerializer
+from reviews_app.models import Review
 
-# Create your views here.
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['update', 'partial_update']:
+            return ReviewUpdateSerializer
+        return ReviewSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(reviewer=self.request.user)
